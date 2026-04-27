43b8abecf0c32ad061cfe50a055bfb25f5daf52c
caromain <romain.carrere@ynov.com>
2026-04-27 17:01:48 +0200

    feat: add configuration file

---
+++ src/artemis/simulator/util/Configuration.java
@@ -0,0 +1,6 @@
+package artemis.simulator.util;
+
+public class Configuration {
+    public static final int PRIX_KEROSENE_PAR_TONNE = 1200;
+    public static final double PROBABILITE_ANOMALIE = 0.05;
+}

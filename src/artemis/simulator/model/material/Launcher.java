fdff0ae3bfe41ab636efdeb0a1d8197d992fe6b2
caromain <romain.carrere@ynov.com>
2026-04-28 17:25:27 +0200

    feat(launcher): add private final class en builder

--- src/artemis/simulator/model/material/Launcher.java
+++ src/artemis/simulator/model/material/Launcher.java
@@ -3,5 +3,12 @@ package artemis.simulator.model.material;
 import artemis.simulator.model.material.capsule.Capsule;
 
 public class Launcher {
-    
+
+    private final String name;
+    private final Capsule capsule;
+
+    public Launcher(String name, Capsule capsule) {
+        this.name = name;
+        this.capsule = capsule;
+    }
 }

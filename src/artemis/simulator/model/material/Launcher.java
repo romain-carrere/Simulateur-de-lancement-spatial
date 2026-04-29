5c14566af9cc91bbcd3551844943ba5edd57c88a
caromain <romain.carrere@ynov.com>
2026-04-29 02:36:04 +0200

    feat(launcher): add class for addbooster and launcher

--- src/artemis/simulator/model/material/Launcher.java
+++ src/artemis/simulator/model/material/Launcher.java
@@ -1,14 +1,24 @@
 package artemis.simulator.model.material;
 
 import artemis.simulator.model.material.capsule.Capsule;
+import java.util.List;
+import java.util.ArrayList;
 
 public class Launcher {
 
     private final String name;
     private final Capsule capsule;
+    private final List<Booster> boosters;
 
     public Launcher(String name, Capsule capsule) {
         this.name = name;
         this.capsule = capsule;
+        this.boosters = new ArrayList<>();
     }
+
+    public void addBooster(Booster booster) {
+        this.boosters.add(booster);
+    }
+
+    
 }
